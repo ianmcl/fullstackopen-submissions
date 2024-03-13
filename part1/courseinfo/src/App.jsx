8a3e347fb208eb72ee992957cce1cb6522f8ffcc
Ian McLaughlin <ianmcl@users.noreply.github.com>
2024-03-13 14:02:02 -0400

    course information, step1
    Total component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,6 +18,14 @@ const Content = (props) => {
   );
 };
 
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -38,7 +46,11 @@ const App = () => {
         part3={part3}
         exercises3={exercises3}
       />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
