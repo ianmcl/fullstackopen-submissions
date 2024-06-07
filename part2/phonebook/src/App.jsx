966f22423bb5b33d09f1fba4dfb243f227420365
Ian McLaughlin <ianmcl@users.noreply.github.com>
2024-06-07 13:00:22 -0400

    2.17: Phonebook step 12
    revised

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -32,7 +32,7 @@ const PersonForm = ({ persons, setPersons, newPerson, setNewPerson, setNotificat
             setTimeout(() => setNotification({ message: null, type: '' }), 5000)
           })
           .catch(error => {
-            setNotification({ message: `Failed to update ${newPerson.name}: ${error.response.data.error}`, type: 'error' })
+            setNotification({ message: `Failed to update ${newPerson.name}: ${error.response.data}`, type: 'error' })
             setTimeout(() => setNotification({ message: null, type: '' }), 5000)
           })
       }
@@ -51,7 +51,7 @@ const PersonForm = ({ persons, setPersons, newPerson, setNewPerson, setNotificat
           setTimeout(() => setNotification({ message: null, type: '' }), 5000)
         })
         .catch(error => {
-          setNotification({ message: `Failed to add person: ${error.response.data.error}`, type: 'error' })
+          setNotification({ message: `Failed to add person: ${error.response.data}`, type: 'error' })
           setTimeout(() => setNotification({ message: null, type: '' }), 5000)
         })
     }
@@ -84,7 +84,8 @@ const Person = ({ person, setPersons, setNotification }) => {
           setTimeout(() => setNotification({ message: null, type: '' }), 5000)
         })
         .catch(error => {
-          setNotification({ message: `Information of ${person.name} has already been removed from server`, type: 'error' })
+          console.log(error.response.data)
+          setNotification({ message: `Failed to delete ${person.name}: ${error.response.data}`, type: 'error' })
           setTimeout(() => setNotification({ message: null, type: '' }), 5000)
         })
     }
@@ -120,7 +121,7 @@ const App = () => {
         setPersons(initialPersons)
       })
       .catch(error => {
-        setNotification({ message: `Failed to fetch persons: ${error.response.data.error}`, type: 'error' })
+        setNotification({ message: `Failed to fetch persons: ${error.response.data}`, type: 'error' })
         setTimeout(() => setNotification({ message: null, type: '' }), 5000)
       })
   }, [])
