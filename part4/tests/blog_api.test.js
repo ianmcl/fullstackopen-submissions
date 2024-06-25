662d8e8698f5f3b58a9c4e6749079591663588a5
Ian McLaughlin <ianmcl@users.noreply.github.com>
2024-06-25 18:26:23 -0400

    4.10: Blog List Tests, step 3
    first part

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -28,12 +28,35 @@ test('there are two blogs', async () => {
   assert.strictEqual(response.body.length, 2)
 })
 
-test.only('blogs have property id instead of _id', async () => {
+test('blogs have property id instead of _id', async () => {
   const response = await api.get('/api/blogs')
   const firstBlog = response.body[0]
 
   assert.ok(firstBlog.id)
   assert.strictEqual(firstBlog._id, undefined)
+})
+
+test.only('creating a new blog post', () => {
+  const newBlog = {
+    title: 'New Test Blog',
+    author: 'Test Author',
+    url: 'http://testurl.com',
+    likes: 10
+  }
+
+  return api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+    .then(() => {
+      return api.get('/api/blogs')
+        .then(response => {
+          const blogsAfterPost = response.body
+          assert.strictEqual(blogsAfterPost.length, helper.initialBlogs.length + 1)
+          const titles = blogsAfterPost.map(blog => blog.title)
+          assert.ok(titles.includes(newBlog.title))
+        })
+    })
 })
 
 after(async () => {
